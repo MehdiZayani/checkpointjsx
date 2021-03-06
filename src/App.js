3cb49089c2a6fb29b19eaceb0a4178417b20d285
MehdiZayani <77887730+MehdiZayani@users.noreply.github.com>
2021-03-06 15:29:10 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
+import mehdi from './imageInSrc.jpg';
 import './App.css';
-
+import video from './myVideo.mp4';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <div className="test">
+
+<h1 className="titleRed">Mehdi</h1>
+
+
+
+<img src={mehdi} alt="heyy"/>
+
+
+
+<img src="/imageInPublic.png"/>
+
+</div>
+
+<video width="620" height="440" controls>
+<source src={video} type="video/mp4" />
+
+</video>
       </header>
     </div>
   );
 }
-
 export default App;
+
